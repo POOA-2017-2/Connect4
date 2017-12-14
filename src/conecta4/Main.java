24082173ad65028e294e7836e77dd48d9bc91303
Jesus Espinoza T. <31456679+jespinoza7@users.noreply.github.com>
2017-12-13 17:34:34 -0800

    La logica queda ya casi terminada, las funciones ya estan probadas.

---
+++ src/conecta4/Main.java
@@ -0,0 +1,19 @@
+package conecta4;
+
+import java.awt.EventQueue;
+
+import javax.swing.JPanel;
+
+public class Main {
+	private static Menu frame;
+
+
+	public static void main(String[] args) {
+		{
+					frame = new Menu();
+					frame.setVisible(true);
+		
+		}
+
+	}
+}
